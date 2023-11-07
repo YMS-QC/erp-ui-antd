150aa9eb3caecc0169f9da4530d08f04a2817a4f
YMS-QC <quacimodoxz@gmail.com>
2023-11-07 21:56:06 +0800

    fix tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './app.tsx';
-import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import './styles/index.css';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <App />
     </React.StrictMode>,
